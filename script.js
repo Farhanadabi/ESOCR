c24a266caf3d1463c64c1db0e27d3ab18af75b83
Farhan Adabi <103530406+Farhanadabi@users.noreply.github.com>
2025-10-07 02:06:22 +0330

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     try {
-      const response = await fetch('https://eso-form-handler.farhan-r-20139021.workers.dev/ ', {
+      const response = await fetch('https://eso-form-handler.farhan-r-20139021.workers.dev/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
@@ -91,3 +91,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
